2774f785ef96e860f409ef0f5ed6731767c5ac4e
Barath P <p.barath1374@gmail.com>
2026-01-18 22:21:12 +0530

    Deployed Successfully !

--- src/main/java/com/justsend/justsendbackend/config/WebConfig.java
+++ src/main/java/com/justsend/justsendbackend/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://justsend-frontend.onrender.com", "http://localhost:5173")
+                .allowedOrigins("https://justsend-fs.onrender.com", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
